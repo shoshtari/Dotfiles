f3fc517576c08a46c46481067b9d90d33412bab2
shoshtari <mr.malekey@gmail.com>
2024-08-30 16:17:52 +0330

    feat: added ollama for nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -11,5 +11,39 @@ return {
 		{
 			"github/copilot.vim",
 		},
+		{
+			"nomnivore/ollama.nvim",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+			},
+
+			-- All the user commands added by the plugin
+			cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+
+			keys = {
+				-- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+				{
+					"<leader>oo",
+					":<c-u>lua require('ollama').prompt()<cr>",
+					desc = "ollama prompt",
+					mode = { "n", "v" },
+				},
+
+				-- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+				{
+					"<leader>oG",
+					":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+					desc = "ollama Generate Code",
+					mode = { "n", "v" },
+				},
+			},
+
+			---@type Ollama.Config
+			opts = {
+				-- your configuration overrides
+				model = "moondream",
+				-- model = "llama3.1:latest"
+			},
+		},
 	},
 }
