bee90d628f41ab433678424aee5d01f578d14e5c
mr.malekey <mr.malekey@gmail.com>
2025-08-04 20:13:39 +0330

    feat: Add copilot chat

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,53 +1,24 @@
 return {
-	-- {
-	-- 	"Exafunction/codeium.nvim",
-	-- 	requires = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"hrsh7th/nvim-cmp",
-	-- 	},
-	-- 	config = function()
-	-- 		require("codeium").setup({})
-	-- 	end,
-	-- },
-
 	{
-		"github/copilot.vim",
-		config = function()
-			-- print something
-			vim.g.copilot_proxy="localhost:2080"
-			-- vim.g.copilot_proxy_strict_ssl=true
-		end,
+		{
+			"github/copilot.vim",
+			config = function()
+				-- print something
+				vim.g.copilot_proxy = "localhost:2080"
+				-- vim.g.copilot_proxy_strict_ssl=true
+			end,
+		},
+		{
+			"CopilotC-Nvim/CopilotChat.nvim",
+			dependencies = {
+				{ "nvim-lua/plenary.nvim", branch = "master" },
+			},
+			build = "make tiktoken",
+			opts = {
+				-- See Configuration section for options
+				-- set a proxy for the copilot chat 
+				proxy = "socks5h://localhost:2080",
+			},
+		},
 	},
-	{
-		"nomnivore/ollama.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-
-		-- All the user commands added by the plugin
-		cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
-
-		keys = {
-			-- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
-			{
-				"<leader>oo",
-				":<c-u>lua require('ollama').prompt()<cr>",
-				desc = "ollama prompt",
-				mode = { "n", "v" },
-			},
-
-			-- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
-			{
-				"<leader>oG",
-				":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
-				desc = "ollama Generate Code",
-				mode = { "n", "v" },
-			},
-		},
-
-		---@type Ollama.Config
-		opts = {
-			model = "deepseek-coder-v2"
-		}
-	}
 }
